d02caa65b497a402fc7e49623adb228dd905aa21
Jane Yang <haewonya@usc.edu>
2018-06-12 19:44:28 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,15 @@
 # Write your #display_rainbow method here
+
+red = "R:red"
+orange = "O: orange" 
+yellow = "Y: yellow"
+green = "G: green"
+blue = "B: blue"
+indigo = "I: indigo"
+violet = "V: violet"
+
+def display_rainbow 
+   puts "#{ red}, #{orange}, #{yellow}, #{green}, #{blue}, #{indigo}, #{violet}"
+
+ 
+ end
